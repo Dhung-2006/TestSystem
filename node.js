5b3140c11e7f9b16c27d4f91c43e8db80da0bd6b
Dhung-2006 <dexterplayer2007@gmail.com>
2025-04-04 15:59:43 +0800

    add requirements

--- node.js
+++ node.js
@@ -1,1 +1,5 @@
-console.log("test output")
+const xlsx = require("xlsx")
+const file_path = "./1.中壢高商(14901).xlsx"
+
+const fileContent  = xlsx.readFile(file_path)
+console.log(fileContent.SheetNames[0])
