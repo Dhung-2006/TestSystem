0bbf80546437b82b2661839e1fd3d82ffec94376
Dhung-2006 <dexterplayer2007@gmail.com>
2025-05-10 15:26:38 +0800

    process update

--- process.py
+++ process.py
@@ -4,15 +4,45 @@ import numpy as np
 def getDataTable(filePath):
     fullTestTable = pd.read_excel(filePath, sheet_name='套印用資料-全測')
     getData(filePath,fullTestTable)
-    studyTestTable = pd.read_excel(filePath, sheet_name='套印用資料-免學')
-    getData(filePath,studyTestTable)
-    technicalTestTable = pd.read_excel(filePath, sheet_name='套印用資料-免術')
-    getData(filePath,technicalTestTable)
+    # studyTestTable = pd.read_excel(filePath, sheet_name='套印用資料-免學')
+    # getData(filePath,studyTestTable)
+    # technicalTestTable = pd.read_excel(filePath, sheet_name='套印用資料-免術')
+    # getData(filePath,technicalTestTable)
     
 def getMajorCode(filePath):
     dataframe= pd.read_excel(filePath,sheet_name='代號',usecols=[0])
     majorLst = [data[0] for data in dataframe.values]
     return majorLst
+
+def testTypeCode(filePath):
+    dataframe = pd.read_excel(filePath , sheet_name="代號"  , usecols=[15])
+    testTypeLst = [data[0] for data in dataframe.values]
+    return testTypeLst
+
+def classCode(filePath):
+    dataframe = pd.read_excel(filePath , sheet_name= "代號" , usecols= [4])
+    classCodeLst = [data[0] for data in dataframe.values]
+    return classCodeLst
+
+def getSchCode(filePath):
+    dataframe = pd.read_excel(filePath,sheet_name="代號" , usecols=[8,9,10])
+    SchCodeLst = [data[1] for data in dataframe.values]
+    return SchCodeLst
+
+def getStudyType(filePath):
+    dataframe = pd.read_excel(filePath ,sheet_name="代號" , usecols=[18])
+    StudyType = [data[0] for data in dataframe.values]
+    return StudyType
+
+def getSpecificCode(filePath):
+    dataframe = pd.read_excel(filePath , sheet_name="代號" , usecols=[21])
+    SpecificLst = [ data[0] for data in dataframe.values]
+    return SpecificLst
+
+def getSchoolCode( filePath):
+    dataframe = pd.read_excel(filePath , sheet_name="代號" , usecols=[12])
+    schoolTypeLst = [data[0] for data in dataframe.values]
+    return schoolTypeLst
 
 def getData(filePath,dataFrame):
     dataRows = dataFrame.shape[0]
@@ -23,25 +53,58 @@ def getData(filePath,dataFrame):
         birthYear = str(dataFrame.loc[i,'出生年'])
         birthMounth = str(dataFrame.loc[i,'出生月'])
         birthDay = str(dataFrame.loc[i, '出生日'])
+        testTypeLst = testTypeCode(filePath)    
+        testTypeID = int(dataFrame.loc[i,"測驗類別"])
+        testType = testTypeLst[testTypeID-1]
+        engName = str(dataFrame.loc[i,"英文姓名"])
+        testSubject = testType[:-2]
+        testType  = testType[-2:]
+        callAdr = str(dataFrame.loc[i,"通訊地址"])
+        liveAdr = str(dataFrame.loc[i,"戶籍地址"])
+        teleArea  = str(dataFrame.loc[i , "電話(區域號碼)"])
+        teleNum = str(dataFrame.loc[i,"電話"])
+        cellNum = str(dataFrame.loc[i,"行動電話"])
+        schCode = str(dataFrame.loc[i,"學校"])
+        SchCodeLst = getSchCode(filePath)
+        stdSch = SchCodeLst[int(schCode)-1]
+        majorCodeLst = getMajorCode(filePath)
+        majorCode = int(dataFrame.loc[i,"科系"]) 
+        major = majorCodeLst[majorCode-1]
+        StudyTypeLst = getStudyType(filePath)
+        studyCode = int(dataFrame.loc[i, "學制"])
+        studyType  = StudyTypeLst[studyCode-1]
+        year = str(dataFrame.loc[i, "年級"])
+        sclass = str(dataFrame.loc[i,"班級"])
+        number  = str(dataFrame.loc[i,"座號"])
+        specificLst = getSpecificCode(filePath)
+        specificCode = int(dataFrame.loc[i,"特定對象"])
+        specificType = "無"
+        specificType = specificLst[specificCode-1 ]if specificCode!=0 else "無"
+        schoolTypeCode = dataFrame.loc[i,"學制"]
+        schoolTypeLst = getSchoolCode(filePath)
+        schoolType = schoolTypeLst[schoolTypeCode]
         datajson = {
             '身分證號碼':identification,
             '中文姓名':chName,
             '出生日期':birthYear + birthMounth + birthDay,
-            '報簡職類':'',
-            '英文姓名':'',
-            '戶籍地址':'',
-            '聯絡電話(住宅)':'',
-            '聯絡電話(手機)':'',
-            '就讀學校':'',
-            '就讀科系':'',
-            '上課別':'',
-            '年級':'',
-            '班級':'',
-            '座號':'',
-            '身分別':'',
-            '學制':''
+            '報簡職類':testSubject,
+            '英文姓名':engName,
+            '檢定區別':testType,
+            '通訊地址':callAdr,
+            '戶籍地址':liveAdr,
+            '聯絡電話(住宅)':"0" + teleArea + teleNum,
+            '聯絡電話(手機)':cellNum,
+            '就讀學校':stdSch,
+            '就讀科系':major,
+            '上課別':studyType,
+            '年級':year,
+            '班級':sclass,
+            '座號':number,
+            '身分別':specificType,
+            '學制':schoolType
         }
-        pass
+        dataJsonLst.append(datajson)
+    print(dataJsonLst)
 
 filePath= "./convert_content/1.中壢高商(14901).xlsx"
-getClassCode(filePath)
+getDataTable(filePath)
